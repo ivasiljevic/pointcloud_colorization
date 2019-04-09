cb1d35eba6bb6f8062fc39b0c6615dff0b958a91
Mohammadreza Mostajabi <reza@gpu23.ttic.edu>
2019-04-09 14:09:28 -0500

    colorization pretraining

--- colorization_pretrain.py
+++ colorization_pretrain.py
@@ -12,6 +12,7 @@ import numpy as np
 from pdb import set_trace as bp
 from torch_geometric.nn import EdgeConv, knn_graph, global_max_pool
 from skimage import color
+import torch.nn as nn
 REPORT_RATE = 100
 NUM_POINTS = 1024
 
@@ -39,7 +40,7 @@ class Net(torch.nn.Module):
 
         self.lin1 = Lin(832, 256)
         self.lin2 = Lin(256, 256)
-        self.lin3 = Lin(256, num_classes)
+        # self.lin3 = Lin(256, num_classes)
 
     def forward(self, data):
 
@@ -69,31 +70,63 @@ class Net(torch.nn.Module):
         x = F.relu(self.lin1(concat_features))
         x = F.relu(self.lin2(x))
         x = F.dropout(x, p=0.5, training=self.training)
-        x = self.lin3(x)
+        # x = self.lin3(x)
+        return x
+
+
+class regressor(torch.nn.Module):
+    def __init__(self, num_classes):
+        super(regressor, self).__init__()
+        self.reg = Lin(256, num_classes)
+
+    def forward(self, x):
+        x = self.reg(x)
+        return x
+
+
+
+class classifier(torch.nn.Module):
+    def __init__(self, num_classes):
+        super(classifier, self).__init__()
+        self.cls = Lin(256, num_classes)
+
+    def forward(self, x):
+        x = self.cls(x)
         return F.log_softmax(x, dim=-1)
+
 
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 #model = Net(23)#.float()#to(device)
-num_classes = 23
-model = Net(23).to(device)
+num_classes = 3
+model = Net(num_classes).to(device)
+reg = regressor(num_classes).to(device)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
+optimizer = torch.optim.Adam([{'params': reg.parameters()},{'params': model.parameters()}], lr=0.001)
+MSEloss = nn.MSELoss()
 def train(epoch):
     count = 0
     model.train()
+    reg.train()
 
 
     for data in train_loader:
-        bp()
+        
+        tmp = np.reshape(data.x[:,0:3], (2, int(data.x.shape[0]/2), 3))
+        tmp = color.rgb2lab(tmp)
+        lbllab = np.reshape(tmp, (data.x.shape[0], 3))
+        # bp()
+        lbllab = torch.from_numpy(lbllab).to(device).float()
         data = data.to(device)
+
         optimizer.zero_grad()
         
-        seg_pred = model(data)
-
-        loss = F.nll_loss(seg_pred, data.y)
+        color_pred = model(data)
+        color_pred = reg(color_pred)
+        # bp()
+        # loss = F.nll_loss(seg_pred, data.y)
+        loss = MSEloss(color_pred,lbllab)
 
         if count % REPORT_RATE == 0:
             print(epoch, loss.data.cpu(),flush=True)
@@ -126,5 +159,7 @@ def test(loader):
 
 for epoch in range(1, 201):
     train(epoch)
-    val_acc = test(val_loader)
-    print('Epoch: {:02d}, Test: {:.4f}'.format(epoch, val_acc))
+    torch.save(model,"basemodel.pkl")
+    torch.save(reg,"reg.pkl")
+    # val_acc = test(val_loader)
+    # print('Epoch: {:02d}, Test: {:.4f}'.format(epoch, val_acc))
